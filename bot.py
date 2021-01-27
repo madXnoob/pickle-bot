fde434d6029338f028c7282e5e119bd848498a6e
madXnoob <77858989+madXnoob@users.noreply.github.com>
2021-01-27 16:31:09 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ async def on_message(message):
             'Yeah well the Scotts beat the vikings soooo','scotts are better then vikings','scotts literally and unironically kicked the vikings arses','Although the Battle of Largs was apparently not considered a significant event by contemporaries, later historians transformed it into an event of international importance. Today, most scholars no longer subscribe to such a view, and instead accord it just an important place in the failed Norwegian campaign.'
         ]
         response = random.choice(responses)
-    if 'midget' or 'midgets' in message.content.lower():
+    elif 'midget' or 'midgets' in message.content.lower():
 
         responses = [
             'I fucking hate midgets'
